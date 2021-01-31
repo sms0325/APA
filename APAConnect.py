20b849ae082e3a1fc3909174d0a91f92b615f743
sms0325 <sms0325@tamu.edu>
2021-01-31 01:19:45 -0600

    comments with next steps.

--- APAConnect.py
+++ APAConnect.py
@@ -10,12 +10,15 @@ def connectToSQLite_new(name, time):
         print("Cannot connect to database.")
     cur = conn.cursor()
 
-    cur.execute("CREATE TABLE APATable (Name character(100), Time timestamp, Type char)")
+    cur.execute("CREATE TABLE IF NOT EXISTS APATable (Name character(100), Time timestamp, Type char)")
     cur.execute("INSERT INTO APATable (Name, Time, Type) VALUES (?, ?, ?)", (name, time, 'r'))
 
     conn.commit()
     cur.close()
     conn.close()
+
+    #this function can be called from APAManager.py, so that way the variables
+    #can be passed into here and sent to the database. 
 
 def connectToSQLite(name, time, type):
     try: 
@@ -25,8 +28,11 @@ def connectToSQLite(name, time, type):
         print("Cannot connect to database.")
     cur = conn.cursor()
 
+    #if-elif statements for different types
+        #cur.execute("INSERT INTO APATable (Name, Time, Type) VALUES (?, ?, ?)", (name, time, 'r'))
+        #different executions depending on context
+
+    #should certain commands indicate to go back to APAManager and trigger something?
+
     cur.close()
     conn.close()
-
-if __name__ == "__main__":
-    connectToSQLite_new()
--- README.md
+++ README.md
@@ -8,4 +8,8 @@ tamuHACK 2021 Project - Dell's prompt (APA: Aggressive Personal Assistant)
 Transparent tkinter background: 
     https://stackoverflow.com/questions/19080499/transparent-background-in-a-tkinter-window
 
+SQL data types: https://www.w3schools.com/sql/sql_datatypes.asp
+
+Setting up SQLite server: https://flaviocopes.com/sqlite-how-to-install/
+
 Cherry boi drawings made by Ymai Nguyen. 
