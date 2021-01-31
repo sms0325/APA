c040b869b8f22a891d1ee109a651d0931554927e
Ninjaneer324 <kakashiandflute@gmail.com>
2021-01-30 21:58:52 -0600

    yes function

---
+++ APAReminder.py
@@ -0,0 +1,11 @@
+import time
+class Reminder:
+    def __init__(self, title,snooze,rtype,window=""):
+        self.title = title
+        self.snooze = snooze
+        self.rtype = rtype
+        self.window = window
+        self.done = False
+    
+    def yes():
+        self.done = True
